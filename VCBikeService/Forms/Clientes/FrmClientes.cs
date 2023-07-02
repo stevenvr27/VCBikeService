8563185bda1b76f85a99349dd98e8a69a79989fa
steven vargas <114686723+stevenvr27@users.noreply.github.com>
2023-07-02 16:10:17 -0600

    a

--- VCBikeService/Forms/Clientes/FrmClientes.cs
+++ VCBikeService/Forms/Clientes/FrmClientes.cs
@@ -12,9 +12,58 @@ namespace VCBikeService.Forms.Clientes
 {
     public partial class FrmClientes : Form
     {
+        private Logic.Models.Customer newcustomer {get; set;}
+        private DataTable CustomerList { get; set;}
+
         public FrmClientes()
         {
             InitializeComponent();
+            CustomerList = new DataTable();
+            newcustomer = new Logic.Models.Customer();
+        }
+
+        private void FrmClientes_Load(object sender, EventArgs e)
+        {
+            FrmClientes frmClientes = new FrmClientes();
+            LoadTypeCustomer();
+        }
+
+        private void LoadTypeCustomer()
+        {
+            Logic.Models.CustomerType customerType = new Logic.Models.CustomerType();
+            DataTable a = new DataTable();
+            a = customerType.ListCustomerType();
+
+            if (a != null && a.Rows.Count>0)
+            {
+                CbTypeCustomer.ValueMember = "ID";
+                CbTypeCustomer.DisplayMember = "Descrip";
+                CbTypeCustomer.DataSource = a;
+                CbTypeCustomer.SelectedIndex = -1;
+            }
+        }
+
+        private void BtnCancelC_Click(object sender, EventArgs e)
+        {
+            this.Hide();
+        }
+
+       
+        
+        private void CleanForm()
+        {
+            TxtCustomerAddress.Clear();
+            TxtCustomerEmail.Clear();
+            TxtCustomerID.Clear();
+            TxtCustomerName.Clear();
+            TxtPhonesCustomer.Clear();
+            CbTypeCustomer.SelectedIndex = -1;
+        }
+
+        private void BtnEditC_Click(object sender, EventArgs e)
+        {
+            CleanForm();
+            DgCustList.ClearSelection();
         }
     }
 }
